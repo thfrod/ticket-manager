3d0ee28062108a0de4e5e1ea0e4e2fdd241dc99e
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-11-16 21:25:17 -0300

    fix: conserta edição de produto

--- administrador/editar_produto.php
+++ administrador/editar_produto.php
@@ -27,6 +27,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $stmtCategoria = $pdo->prepare("SELECT CATEGORIA_NOME, CATEGORIA_ID FROM CATEGORIA WHERE CATEGORIA_ATIVO = 1");
             $stmtCategoria->execute();
             $categorias = $stmtCategoria->fetchAll(PDO::FETCH_ASSOC);
+
+
+            $stmtImagens = $pdo->prepare("SELECT * FROM PRODUTO_IMAGEM WHERE PRODUTO_ID = :id");
+            $stmtImagens->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmtImagens->execute();
+            $imagens = $stmtImagens->fetchAll(PDO::FETCH_ASSOC); //Recupera todos os registros retornados pela consulta SQL e os armazena na variável $produtos como um array associativo, onde as chaves do array são os nomes das colunas da tabela PRODUTOS
+
         } catch (PDOException $e) {
             echo "Erro: " . $e->getMessage();
         }
@@ -38,27 +45,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 // Se o formulário de edição foi submetido, a página é acessada via método POST, e o script tenta atualizar os detalhes do produto no banco de dados com as informações fornecidas no formulário.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id = $_POST['PRODUTO_ID'];
-    $nome = $_POST['PRODUTO_NOME'];
-    $descricao = $_POST['PRODUTO_DESC'];
-    $preco = $_POST['PRODUTO_PRECO'];
-    $url_imagem = $_POST['url_imagem'];
+    $id = $_POST['id'];
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    $desconto = $_POST['desconto'];
+    $ativo = $_POST['status'];
+    $categoria = $_POST['categoria'];
+    $imagens = $_POST['imagem_url'];
+    $imagensId = $_POST['imagem_id'];
+
+
 
     try {
-        $stmt = $pdo->prepare("UPDATE PRODUTOS SET PRODUTO_NOME = :nome, PRODUTO_DESC = :descricao, PRODUTO_PRECO = :preco, URL_IMAGEM = :url_imagem WHERE ID = :id");
+        $stmt = $pdo->prepare("UPDATE PRODUTO SET PRODUTO_NOME = :nome, PRODUTO_DESC = :descricao, PRODUTO_PRECO = :preco, PRODUTO_DESCONTO = :desconto, CATEGORIA_ID = :categoria, PRODUTO_ATIVO = :ativo WHERE PRODUTO_ID = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
         $stmt->bindParam(':descricao', $descricao, PDO::PARAM_STR);
         $stmt->bindParam(':preco', $preco, PDO::PARAM_STR);
-        $stmt->bindParam(':url_imagem', $url_imagem, PDO::PARAM_STR);
+        $stmt->bindParam(':desconto', $desconto, PDO::PARAM_STR);
+        $stmt->bindParam(':categoria', $categoria, PDO::PARAM_INT);
+        $stmt->bindParam(':ativo', $ativo, PDO::PARAM_BOOL);
         $stmt->execute();
+
+        foreach ($imagens as $ordem => $url_imagem) {
+            $sql_imagem = "UPDATE PRODUTO_IMAGEM SET IMAGEM_URL = :url_imagem WHERE PRODUTO_ID = :id AND IMAGEM_ID = :imagem_id";
+            $stmt_imagem = $pdo->prepare($sql_imagem);
+            $stmt_imagem->bindParam(':url_imagem', $url_imagem, PDO::PARAM_STR);
+            $stmt_imagem->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt_imagem->bindParam(':imagem_id', $imagensId[$ordem], PDO::PARAM_INT);
+            $stmt_imagem->execute();
+        }
 
         header("Location:listar_produtos.php?success=Produto atualizado com successo");
 
         exit();
-
     } catch (PDOException $e) {
-        header("Location:listar_produtos.php?error=Erro ao atualizar produto");
+        echo "Erro: " . $e->getMessage();
+        // header("Location:listar_produtos.php?error=Erro ao atualizar produto");
     }
 }
 ?>
@@ -76,13 +100,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <input type="hidden" name="id" value="<?php echo $produto['PRODUTO_ID']; ?>">
 
             <div class="input-group mb-3">
-                <input class="form-control" type="text" name="nome" id="nome" required placeholder="Nome"
-                    value="<?php echo $produto['PRODUTO_NOME']; ?>">
+                <input class="form-control" type="text" name="nome" id="nome" required placeholder="Nome" value="<?php echo $produto['PRODUTO_NOME']; ?>">
             </div>
 
             <div class="input-group mb-3">
-                <input class="form-control" type="text" name="descricao" id="descricao" required placeholder="Descrição"
-                    value="<?php echo $produto['PRODUTO_DESC']; ?>">
+                <input class="form-control" type="text" name="descricao" id="descricao" required placeholder="Descrição" value="<?php echo $produto['PRODUTO_DESC']; ?>">
             </div>
 
             <div class="input-group mb-3">
@@ -99,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="input-group mb-3">
                 <select class="form-control" name="categoria" id="categoria" required>
                     <option value="0" selected disabled>Selecione uma categoria</option>
-                    <?php foreach ($categorias as $categoria): ?>
+                    <?php foreach ($categorias as $categoria) : ?>
                         <option value="<?php echo $categoria['CATEGORIA_ID'] ?>" <?php echo $produto['CATEGORIA_ID'] == $categoria['CATEGORIA_ID'] ? 'selected' : ''; ?>>
                             <?php echo $categoria['CATEGORIA_NOME'] ?>
                         </option>
@@ -108,24 +130,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
 
             <div class="input-group mb-3">
-                <input class="form-control" type="number" name="preco" id="preco" step="0.01" required
-                    placeholder="Preço" value="<?php echo $produto['PRODUTO_PRECO']; ?>">
+                <input class="form-control" type="number" name="preco" id="preco" step="0.01" required placeholder="Preço" value="<?php echo $produto['PRODUTO_PRECO']; ?>">
             </div>
 
             <div class="input-group mb-3">
-                <input class="form-control" type="number" name="desconto" id="desconto" step="0.01" required
-                    placeholder="Desconto" value="<?php echo $produto['PRODUTO_DESCONTO']; ?>">
+                <input class="form-control" type="number" name="desconto" id="desconto" step="0.01" required placeholder="Desconto" value="<?php echo $produto['PRODUTO_DESCONTO']; ?>">
             </div>
 
+            <div class="container-imgs">
+                <?php foreach ($imagens as $imagem) : ?>
+                    <div class="d-flex">
+                        <div class="input-group mb-3">
+                            <input type="hidden" name="imagem_id[]" value="<?php echo $imagem['IMAGEM_ID']; ?>">
+                            <input class="form-control" type="text" name="imagem_url[]" id="imagem" required placeholder="Imagem" value="<?php echo $imagem['IMAGEM_URL']; ?>">
+                        </div>
+                        <!-- <div class="btn btn-danger rounded-circle p-1 btn-add-img" onclick="removeImagem()">
+                            <span class="material-symbols-outlined text-white">delete</span>
+                        </div> -->
+                    </div>
 
-            AQUI DEVE VIR A LISTA DE IMAGENS RELACIONADAS AQUELE PRODUTO POREM AINDA NÂO FIZ O SELECT JOIN NO BANCO DE
-            DADOS
-            <!-- <div class="input-group mb-3">
-                <input class="form-control" type="text" name="imagem" id="imagem" required placeholder="Imagem" value="<?php echo $produto['PRODUTO_DESC']; ?>">
-            </div> -->
+                <?php endforeach; ?>
+
+                <!-- <div class="d-flex">
+                    <div class="input-group mb-3">
+                        <input class="form-control" type="text" name="imagem_url[]" required placeholder="Imagem">
+                    </div>
+                    <div class="btn btn-primary rounded-circle p-1 btn-add-img" onclick="adicionarImagem()">
+                        <span class="material-symbols-outlined text-white">add</span>
+                    </div>
+                </div> -->
+            </div>
 
             <input type="submit" value="Cadastrar" class="btn btn-success">
 
         </form>
     </div>
+    <script>
+        function adicionarImagem() {
+            const containerImagens = document.querySelector('.container-imgs');
+            const novoInput = document.createElement('div');
+            novoInput.classList.add('input-group', 'mb-3');
+            novoInput.innerHTML = `<input class="form-control" type="text" name="imagem_url[]" required placeholder="Imagem">`;
+            containerImagens.appendChild(novoInput);
+        }
+
+        function removeImagem() {
+            event.preventDefault();
+            event.target.parentNode.parentNode.remove();
+        }
+    </script>
 </body>
