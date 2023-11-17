edc27d91ae7d5685b689b8bba03730896ff5a1a3
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-11-16 21:30:55 -0300

    feat: change aside category icon

--- shared/aside.php
+++ shared/aside.php
@@ -1,7 +1,6 @@
 <div class="aside-content col-auto col-md-1 col-xl-2 px-sm-2 px-0 bg-primary">
     <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-        <a href="painel_admin.php"
-            class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <a href="painel_admin.php" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
             <img src="../assets/imgs/logo.svg" alt="" class="menu-logo">
         </a>
         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
@@ -15,7 +14,7 @@
                     <span class="mt-1 ms-2 d-none d-sm-inline text-white">Produtos</span>
                 </a>
                 <a href="listar_categorias.php" class="nav-link align-middle px-0 d-flex align-items-center">
-                    <span class="material-symbols-outlined text-white">list</span>
+                    <span class="material-symbols-outlined text-white">category</span>
                     <span class="mt-1 ms-2 d-none d-sm-inline text-white">Categorias</span>
                 </a>
                 <a href="listar_adms.php" class="nav-link align-middle px-0 d-flex align-items-center">
